0f79b648e1c203020fae87b8dea3b3d6199bb30a
skoltun <sebastian.koltun7@gmail.com>
2019-02-01 00:53:52 +0100

    v1.2 added login response

--- src/main/java/pl/skoltun/spring/security/rest/adapter/RestLoginSuccessHandler.java
+++ src/main/java/pl/skoltun/spring/security/rest/adapter/RestLoginSuccessHandler.java
@@ -1,15 +1,39 @@
 package pl.skoltun.spring.security.rest.adapter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import pl.skoltun.spring.security.rest.login.LoginResponse;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.stream.Collectors;
 
 class RestLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private static final Logger logger = Logger.getLogger(RestLoginSuccessHandler.class);
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) {
         clearAuthenticationAttributes(request);
+        LoginResponse loginResponse = buildResponse(authentication);
+        try(OutputStream out = response.getOutputStream()){
+            new ObjectMapper().writer()
+                    .forType(LoginResponse.class)
+                    .writeValue(out,loginResponse);
+        }catch (IOException e){
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+    private LoginResponse buildResponse(Authentication authentication) {
+        LoginResponse response = new LoginResponse();
+        response.setUsername(authentication.getName());
+        response.setAuthorities(authentication.getAuthorities().stream()
+                .map(String::valueOf)
+                .collect(Collectors.toList()));
+        return response;
     }
 }
---
+++ src/main/java/pl/skoltun/spring/security/rest/login/LoginResponse.java
@@ -0,0 +1,24 @@
+package pl.skoltun.spring.security.rest.login;
+
+import java.util.List;
+
+public class LoginResponse {
+    private String username;
+    private List<String> authorities;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<String> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(List<String> authorities) {
+        this.authorities = authorities;
+    }
+}
